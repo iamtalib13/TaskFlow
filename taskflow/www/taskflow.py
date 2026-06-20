3023eaebf3785dfe214696640a8e1436a89f03f7
iamtalib13 <talibsheikh16@gmail.com>
2026-06-20 14:56:27 +0530

    chore: stage all modified and untracked files

--- taskflow/www/taskflow.py
+++ taskflow/www/taskflow.py
@@ -1,28 +1,12 @@
 import frappe
-from frappe.boot import load_translations
-
-no_cache = 1
 
 
 def get_context(context):
-    csrf_token = frappe.sessions.get_csrf_token()
-    frappe.db.commit()  # nosempgrep
-    context = frappe._dict()
-    context.csrf_token = csrf_token
-    context.site_name = frappe.local.site
-    context.boot = get_boot()
-    context.title = "Taskflow"
-    context.body_class = "taskflow-page"
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login?redirect-to=/taskflow"
+        raise frappe.Redirect
+
+    context.no_cache = 1
+    context.title = "Taskflow Portal"
+    context.body_class = "taskflow-portal-page"
     return context
-
-
-def get_boot():
-    bootinfo = frappe._dict(
-        {
-            "site_name": frappe.local.site,
-            "default_route": "/taskflow",
-        }
-    )
-    bootinfo.lang = frappe.local.lang
-    load_translations(bootinfo)
-    return bootinfo
