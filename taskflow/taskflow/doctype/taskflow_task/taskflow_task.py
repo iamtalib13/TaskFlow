c0752ecc9529ab69e6940e670a0a3227046f8d57
iamtalib13 <talibsheikh16@gmail.com>
2026-06-22 14:54:39 +0530

    feat: update to taskflow

--- taskflow/taskflow/doctype/taskflow_task/taskflow_task.py
+++ taskflow/taskflow/doctype/taskflow_task/taskflow_task.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+
 import frappe
 from frappe.model.document import Document
 from frappe import _
@@ -16,6 +18,7 @@ class TaskflowTask(Document):
 		self._validate_user_access_rules()
 
 	def before_save(self):
+		self._sync_temp_emp_assignments()
 		if not self.start_date:
 			self.start_date = frappe.utils.today()
 		if not self.due_date:
@@ -30,6 +33,48 @@ class TaskflowTask(Document):
 			self.assigned_to_user = frappe.db.get_value("Employee", self.assigned_to, "user_id")
 		else:
 			self.assigned_to_user = None
+
+	def _sync_temp_emp_assignments(self):
+		if not self.temp_emp:
+			return
+
+		employee_ids = self._parse_employee_ids(self.temp_emp)
+		if not employee_ids:
+			return
+
+		user_ids = []
+		for employee_id in employee_ids:
+			user_id = frappe.db.get_value("Employee", employee_id, "user_id")
+			if user_id and user_id not in user_ids:
+				user_ids.append(user_id)
+
+		self._assign = json.dumps(user_ids)
+
+	def _parse_employee_ids(self, value):
+		if not value:
+			return []
+
+		if isinstance(value, (list, tuple)):
+			raw_values = value
+		else:
+			try:
+				parsed_value = frappe.parse_json(value)
+				if isinstance(parsed_value, (list, tuple)):
+					raw_values = parsed_value
+				else:
+					raw_values = [parsed_value]
+			except Exception:
+				raw_values = [
+					part.strip()
+					for part in str(value).replace("\n", ",").replace(";", ",").split(",")
+				]
+
+		employee_ids = []
+		for employee_id in raw_values:
+			employee_id = str(employee_id).strip()
+			if employee_id and employee_id not in employee_ids:
+				employee_ids.append(employee_id)
+		return employee_ids
 
 	def _validate_dates(self):
 		if self.start_date and self.due_date and self.start_date > self.due_date:
