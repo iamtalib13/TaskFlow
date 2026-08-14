411fcf32bcbfdbeec1376af6492efa303b0f86b8
iamfaijank <iamfaijank@gmail.com>
2026-08-14 12:04:33 +0530

    Add custom naming {user}-dd-mm-yyyy and fix hours calculation on save

--- taskflow/taskflow/doctype/taskflow_timesheet/taskflow_timesheet.py
+++ taskflow/taskflow/doctype/taskflow_timesheet/taskflow_timesheet.py
@@ -3,9 +3,20 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import today, getdate
 
 
 class TaskflowTimesheet(Document):
 	def before_insert(self):
 		if not self.user:
 			self.user = frappe.session.user
+
+	def validate(self):
+		for item in self.get("table_pfiw", []):
+			if item.from_time and item.to_time and not item.hrs:
+				item.calculate_hours()
+
+	def autoname(self):
+		user = frappe.get_cached_value("User", self.user, "full_name") or self.user
+		date_str = getdate().strftime("%d-%m-%Y")
+		self.name = f"{user}-{date_str}"
--- taskflow/taskflow/doctype/taskflow_timesheet_item/taskflow_timesheet_item.py
+++ taskflow/taskflow/doctype/taskflow_timesheet_item/taskflow_timesheet_item.py
@@ -3,13 +3,18 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import time_diff_in_hours
+from frappe.utils import get_datetime, time_diff_in_seconds
 
 
 class TaskflowTimesheetItem(Document):
 	def validate(self):
+		frappe.logger().debug(f"TaskflowTimesheetItem validate: from_time={self.from_time}, to_time={self.to_time}")
 		self.calculate_hours()
+		frappe.logger().debug(f"TaskflowTimesheetItem calculated hrs={self.hrs}")
 
 	def calculate_hours(self):
 		if self.from_time and self.to_time:
-			self.hrs = time_diff_in_hours(self.to_time, self.from_time)
+			from_dt = get_datetime(self.from_time)
+			to_dt = get_datetime(self.to_time)
+			self.hrs = time_diff_in_seconds(to_dt, from_dt) / 3600
+			frappe.logger().debug(f"Calculated hours: {self.hrs}")
