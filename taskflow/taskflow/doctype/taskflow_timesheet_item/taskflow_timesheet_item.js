5955f5ff715a1908a1b871966c23e8a3abe6bfdd
iamfaijank <iamfaijank@gmail.com>
2026-08-14 11:53:32 +0530

    Auto-calculate hours from from_time and to_time in Taskflow Timesheet Item

--- taskflow/taskflow/doctype/taskflow_timesheet_item/taskflow_timesheet_item.js
+++ taskflow/taskflow/doctype/taskflow_timesheet_item/taskflow_timesheet_item.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2026, Talib Sheikh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Taskflow Timesheet Item", {
-// 	refresh(frm) {
-//
-// 	},
-// });
+frappe.ui.form.on("Taskflow Timesheet Item", {
+	from_time(frm, cdt, cdn) {
+		calculate_hours(frm, cdt, cdn);
+	},
+	to_time(frm, cdt, cdn) {
+		calculate_hours(frm, cdt, cdn);
+	},
+});
+
+function calculate_hours(frm, cdt, cdn) {
+	const row = frappe.get_doc(cdt, cdn);
+	if (row.from_time && row.to_time) {
+		const diff = frappe.datetime.get_diff(row.to_time, row.from_time);
+		const hours = diff / 3600;
+		frappe.model.set_value(cdt, cdn, "hrs", hours);
+	}
+}
--- taskflow/taskflow/doctype/taskflow_timesheet_item/taskflow_timesheet_item.py
+++ taskflow/taskflow/doctype/taskflow_timesheet_item/taskflow_timesheet_item.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2026, Talib Sheikh and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import time_diff_in_hours
 
 
 class TaskflowTimesheetItem(Document):
-	pass
+	def validate(self):
+		self.calculate_hours()
+
+	def calculate_hours(self):
+		if self.from_time and self.to_time:
+			self.hrs = time_diff_in_hours(self.to_time, self.from_time)
